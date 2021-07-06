59840163635e1c96f54c3f9cf9104e926044bd4b
向井優一 <yuichi-m@weseek.co.jp>
2021-07-06 11:42:47 +0900

    finish bakend api controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
 
   #メソッドに!をつけることで例外処理を出し、使う側が内部処理を把握しやすいようにする
   def save_with_update_line_foods!(line_foods)
+    #破壊的メソッドであるupdate_attributes!とsave!の２つの処理に対してトランザクションを張る
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
         line_food.update_attributes!(active: false, order: self)
