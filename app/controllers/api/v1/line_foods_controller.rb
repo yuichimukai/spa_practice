c87553257a306511f6e1aec195f59f7772dbf585
向井優一 <yuichi-m@weseek.co.jp>
2021-07-10 17:17:27 +0900

    add flow order page

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class LineFoodsController < ApplicationController
-      before_action :set_food, only: %[create replace]
+      before_action :set_food, only: %i[create replace]
 
       def index
         line_foods = LineFood.active
